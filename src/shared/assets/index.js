d0c404e4942e8c4efab1d8915e401c3a0980b277
Ner47 <neogil28@gmail.com>
2026-07-07 17:16:56 +0300

    feat: add link and style for header

---
+++ src/shared/assets/index.js
@@ -0,0 +1,2 @@
+export { default as iconProfile } from "./profile.svg";
+export { default as iconCart } from "./cart.svg";
--- src/widgets/Header/Header.jsx
+++ src/widgets/Header/Header.jsx
@@ -1,9 +1,35 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
+import { iconCart, iconProfile } from "../../shared/assets";
 
 export function Header() {
   return (
     <header className="header">
-      <p>header</p>
+      <div className="header__container">
+        <div className="header__logo">
+          <Link to="/" className="header__home">
+            DAWN
+          </Link>
+        </div>
+        <div className="header__actions">
+          <Link to="/account" className="header__profile">
+            <img
+              className="header__profile"
+              src={iconProfile}
+              alt="profile"
+              aria-hidden="true"
+            />
+          </Link>
+          <Link to="/cart" className="header__profile">
+            <img
+              className="header__profile"
+              src={iconCart}
+              alt="cart"
+              aria-hidden="true"
+            />
+          </Link>
+        </div>
+      </div>
     </header>
   );
 }
