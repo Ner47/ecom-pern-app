9fc3c94fccb81bece69d00f310f3817d2d365c86
Ner47 <neogil28@gmail.com>
2026-07-10 14:07:18 +0300

    chore: update orders page

--- src/features/orders/ui/OrderCard.jsx
+++ src/features/orders/ui/OrderCard.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
+import "./OrderCard.css";
 
 export function OrderCard({ order }) {
   return (
     <article className="order-card">
       <h3>Order #{order.id}</h3>
-      <p>Status: {order.status}</p>
-      <p>Total: ${order.total}</p>
+      <p>
+        Status: <span>{order.status}</span>
+      </p>
+      <p>
+        Total: <span>${order.total}</span>
+      </p>
 
-      <Link to={`/orders/${order.id}`}>View details</Link>
+      <Link className="order-card__link" to={`/orders/${order.id}`}>
+        View details
+      </Link>
     </article>
   );
 }
--- src/pages/OrdersPage/OrdersPage.jsx
+++ src/pages/OrdersPage/OrdersPage.jsx
@@ -1,5 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./OrdersPage.css";
+import { OrderCard, selectOrders, fetchOrders } from "@/features/orders";
+import { useEffect } from "react";
 
 export function OrdersPage() {
-  return "Orders";
+  const dispatch = useDispatch();
+  const orders = useSelector(selectOrders);
+
+  useEffect(() => {
+    dispatch(fetchOrders());
+  }, [dispatch]);
+
+  return (
+    <main className="orders">
+      <h1>Yours Orders</h1>
+
+      {orders.map((order) => (
+        <OrderCard key={order.id} order={order} />
+      ))}
+    </main>
+  );
 }
