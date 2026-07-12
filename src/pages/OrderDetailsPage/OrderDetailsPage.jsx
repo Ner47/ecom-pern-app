f3a9b0b2595f5751ccf99ee582cc6d43793ac88e
Ner47 <neogil28@gmail.com>
2026-07-12 14:29:26 +0300

    chore: update order page

--- src/pages/OrderDetailsPage/OrderDetailsPage.jsx
+++ src/pages/OrderDetailsPage/OrderDetailsPage.jsx
@@ -1,5 +1,46 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./OrderDetailsPage.css";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { fetchOrdersById, selectSelectedOrder } from "@/features/orders";
 
 export function OrderDetailsPage() {
-  return "Order Details";
+  const { orderId } = useParams();
+  const dispatch = useDispatch();
+
+  const order = useSelector(selectSelectedOrder);
+
+  useEffect(() => {
+    dispatch(fetchOrdersById(orderId));
+  });
+
+  if (!order) return <main className="order">Loading order ...</main>;
+
+  return (
+    <main className="order">
+      <h1>Order #{order.id}</h1>
+
+      <p>
+        Status: <span>{order.status}</span>
+      </p>
+      <p>
+        Total: <span>{Number(order.total).toFixed(2)} CAD</span>
+      </p>
+
+      <div className="order-items">
+        {order.items?.map((item) => (
+          <div key={item.id} className="order-items__card">
+            <img
+              className="order-items__image"
+              src={`/src/shared/assets/${item.name}.png`}
+              alt={item.name}
+            />
+            <p className="order-items__name">{item.name}</p>
+            <p>Quantity: {item.qty}</p>
+            <p>Description: {item.description}</p>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
