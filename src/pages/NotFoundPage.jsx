6ac4e8acad930930c92386e8cb40891946d8740c
Ner47 <neogil28@gmail.com>
2026-07-14 18:15:41 +0300

    chore: update not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,3 +1,14 @@
+import { Link } from "react-router-dom";
+import "./NotFoundPage.css";
+
 export function NotFoundPage() {
-  return "404 Page Not Found";
+  return (
+    <main className="not-found">
+      <section className="not-found__card">
+        <h1>404 - Page Not Found</h1>
+        <p>The page does not exist</p>
+        <Link to="/">Go home</Link>
+      </section>
+    </main>
+  );
 }
